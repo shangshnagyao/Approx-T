bd0e8b7d1484d8de156522cdae282ff90e950568
YSS <67365677+shangshnagyao@users.noreply.github.com>
2024-12-28 00:37:37 +0800

    Update tb.v

--- tb/tb.v
+++ tb/tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2021/12/04 20:52:01
-// Design Name: 
-// Module Name: tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: Testbench for unsigned integer multiplication
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module tb;
 
